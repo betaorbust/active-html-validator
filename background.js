4f366848a7d0e8eed455864412ca4051800dd845
Jacques Favreau <jacques@udacity.com>
2013-02-14 14:13:46 -0800

    Just a little cleanup.

--- background.js
+++ background.js
@@ -1,17 +1,3 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// chrome.extension.onConnect.addListener(function(port) {
-//	var tab = port.sender.tab;
-
-//	// This will get called by the content script we execute in
-//	// the tab as a result of the user pressing the browser action.
-//	port.onMessage.addListener(function(info) {
-//		console.log(info.html);
-//	});
-// });
-
 // Called when the user clicks on the browser action icon.
 chrome.browserAction.onClicked.addListener(function(tab) {
 	// We can only inject scripts to find the title on pages loaded with http
